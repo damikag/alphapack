a2ebde4eb48574161cf38edef4df498634d8902b
Achintha444 <achinthaisuru444@gmail.com>
2019-03-03 12:12:08 +0530

    comment.php created

--- Website/Website/Controller/comment.php
+++ Website/Website/Controller/comment.php
@@ -1,10 +1,14 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+include("../Model/customer.php");
+$dbh = new Dbh();
+$customer = new Customer($dbh);
+
+if (isset($_SESSION["userName"])){
+	
+}
+else{
+	header("Location: ../View/promoterTemplate.php?error=pleaseLogin");
+	exit();
+
+}
