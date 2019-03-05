ea8cb210bf02a8c7bc1901a19b0a5a7f518c2a1c
damikag <damika97.10@gmail.com>
2019-03-05 21:05:27 +0530

    Addpromo.php and header.php created

---
+++ Website/View/header.php
@@ -0,0 +1,140 @@
+<?php
+
+	session_start();
+		
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>වාසි.lk-header</title>
+    <link rel="shortcut icon" type="image/ico" href="../Extra/img/favicon.png" />
+	
+    <!--====== STYLESHEETS ======-->
+	<link rel="stylesheet" href="../Extra/css/normalize.css">
+    <link rel="stylesheet" href="Extra/css/normalize.css">
+    <link rel="stylesheet" href="../Extra/css/animate.css">
+    <link rel="stylesheet" href="../Extra/css/stellarnav.min.css">
+    <link rel="stylesheet" href="../Extra/css/owl.carousel.css">
+    <link href="../Extra/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../Extra/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../Extra/css/imagehover.min.css" rel="stylesheet">
+
+    <!--====== MAIN STYLESHEETS ======-->
+	<link rel="stylesheet" href="../Extra/css/style2.css">
+    <link href="style.css" rel="stylesheet">
+    <link href="../Extra/css/responsive.css" rel="stylesheet">
+	<link href="../Extra/css/ihover.css" rel="stylesheet">
+
+    <script src="../Extra/js/vendor/modernizr-2.8.3.min.js"></script>
+    <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body class="home-one">
+
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!--- PRELOADER -->
+    <div class="preeloader">
+        <div class="preloader-spinner"></div>
+    </div>
+
+    <!--SCROLL TO TOP-->
+    <a href="#home" class="scrolltotop"><i class="fa fa-long-arrow-up"></i></a>
+
+    <!--START TOP AREA-->
+    <header class="top-area" id="home">
+        <div class="top-area-bg" data-stellar-background-ratio="0.6"></div>
+        <div class="header-top-area">
+            <!--MAINMENU AREA-->
+            <div class="mainmenu-area" id="mainmenu-area">
+                <div class="mainmenu-area-bg"></div>
+                <nav class="navbar">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a href="#home" class="navbar-brand"><img src="../Extra/img/logo.png" alt="logo"></a>
+                        </div>
+                        <div class="search-and-language-bar pull-right">
+                            <ul>
+								<?php
+									if (! isset($_SESSION["userName"])){
+										echo '<li><a href="login.php"><i class="fa fa-user"></i></a></li>';
+										
+									}
+								?>
+                                
+                                <li class="search-box"><i class="fa fa-search"></i></li>
+                               	</li>
+                            </ul>
+                            <form action="#" class="search-form">
+                                <input type="search" name="search" id="search">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>
+                        <div id="main-nav" class="stellarnav">
+                            <ul id="nav" class="nav navbar-nav">
+								<li><a href="index.php">home</a></li>
+								<li><a href="about.html">about</a>
+                                </li>
+                                <li><a href="service.html">Service</a>
+                                    <ul>
+										<li><a href="promoterLogin.php">Login as a promoter</a></li>
+										<li><a href="promoterTemplate.php">Login as a promoter112</a></li>
+                                    </ul>
+                                </li>
+								<li class="logged-user">
+									<?php
+									
+									if (isset($_SESSION["userName"])){
+										
+										$username = $_SESSION["userName"];
+										echo '<a href="#" class="logged-user" background-colour="lightsalmon">
+									'.$username.'
+									</a>
+                                    <ul>
+                                        <li class="logout-submit"><a href="../Controller/logout.php">Logout</a></li>
+                                        <li><a href="contact-2.html">Contact Version 2</a></li>
+                                    </ul>';
+									}
+									
+									else{
+										echo '<a href="login.php">LOGIN AS A CUSTOMER</a>';		
+									}
+								?>								
+								</li>	
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <!--END MAINMENU AREA END-->
+        </div>
+        <!--HOME SLIDER AREA-->
+        
+
+    <!--====== SCRIPTS JS ======-->
+    <script src="../Extra/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="../Extra/js/vendor/bootstrap.min.js"></script>
+
+    <!--====== PLUGINS JS ======-->
+    <script src="../Extra/js/vendor/jquery.easing.1.3.js"></script>
+    <script src="../Extra/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+    <script src="../Extra/js/vendor/jquery.appear.js"></script>
+    <script src="../Extra/js/owl.carousel.min.js"></script>
+    <script src="../Extra/js/stellar.js"></script>
+    <script src="../Extra/js/wow.min.js"></script>
+    <script src="../Extra/js/stellarnav.min.js"></script>
+    <script src="../Extra/js/contact-form.js"></script>
+    <script src="../Extra/js/jquery.sticky.js"></script>
+
+    <!--===== ACTIVE JS=====-->
+    <script src="../Extra/js/main.js"></script>
+</body>
+
+</html>
