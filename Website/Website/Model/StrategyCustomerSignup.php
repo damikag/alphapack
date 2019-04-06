7a333131799cc67c4685ae51e519597768ab7db5
Achinths Isuru <achinthaisuru444@gmail.com>
2019-04-06 09:02:39 +0530

    * Created the IStrategySignup.php class

---
+++ Website/Website/Model/StrategyCustomerSignup.php
@@ -0,0 +1,117 @@
+<?php
+
+require_once("../Model/IStrategySignup");
+
+class StrategyCustomerSignup implements IStrategySignup{
+	
+	private $dbh;
+	
+	public function __construct(){
+		$this->dbh = new Dbh();
+	}
+	
+	private function signupFunction(){
+		
+		$username = $_POST["uid"];
+		$uemail = $_POST["email"];
+		$password = $_POST["password"];
+		$re_password = $_POST["re-password"];
+		$phone = $_POST["phone"];
+		
+		if(empty($username) || empty($uemail) || empty($password) || empty($re_password) || empty($phone)){
+
+			header("Location: ../View/signup.php?error=emptyFields_&uid=".$username."_&email=".$uemail."_&phone=".$phone);
+			exit();
+		}
+
+		elseif(!preg_match("/^[a-zA-z0-9]*$/", $username)){
+			header("Location: ../View/signup.php?error=invalidUserName_&email=".$uemail."_&phone=".$phone);
+			exit();
+
+		}
+
+		elseif($password !== $re_password){
+			header("Location: ../View/signup.php?error=passwordError_&uid=".$username."_&email=".$uemail."_&phone=".$phone);
+			exit();
+
+		}
+		
+		elseif(strlen($password)<8){
+			header("Location: ../View/signup.php?error=passwordNotStrong_&uid=".$username."_&email=".$uemail."_&phone=".$phone);
+			exit();
+		}
+		
+		elseif(strlen($phone)!=10){
+			header("Location: ../View/signup.php?error=incorrectPhoneNumber_&uid=".$username."_&email=".$uemail);
+			exit();
+		}
+
+		else{
+			
+			$conn = $this->dbh->connect();
+			$sql = $conn->prepare("SELECT * FROM customer WHERE username=?");
+			#$sql = "SELECT username FROM users WHERE username=?";
+			#$stmt = mysqli_stmt_init($conn);
+			$sql->bind_param("s", $username);
+			$sql->execute();
+			$sql->store_result();
+			$resultCheck = $sql->num_rows();
+			
+				#mysqli_stmt_bind_param($stmt, "s", $username);
+				#mysqli_stmt_execute($stmt);
+				#mysqli_stmt_store_result($stmt);
+				#$resultCheck = mysqli_stmt_num_rows($stmt);
+				if($resultCheck > 0){
+					header("Location: ../View/signup.php?error=userNameTaken_&email=".$uemail);
+					exit();
+
+				}
+				else{
+					/*$conn = $dbh->connect();
+					#$sql = "INSERT INTO users(uname, email, password) VALUES (?, ? ,?) ";
+					$sql = $conn->prepare("INSERT INTO promotor(username, password, email,phone_no,promotor_name,website,fb_link) VALUES (?, ? ,?,?,?,?,?) ");
+					#$sql = $conn->prepare("INSERT INTO users(uname, email, password) VALUES (?, ? ,?) ");
+					$hPassword = password_hash($password, PASSWORD_DEFAULT);
+					$hPassword=$password;
+					$sql->bind_param("sssssss", $username,$hPassword,$uemail,$phone,$promoterName,$website,$fblink);
+					$sql->execute();
+					$sql->store_result();
+					
+						/*mysqli_stmt_bind_param($stmt, "sss", $username, $uemail,$hPassword);
+						mysqli_stmt_execute($stmt);
+						mysqli_stmt_store_result($stmt);
+						header("Location: ../View/login.php");
+						exit();*/
+					
+					
+					$conn = $this->dbh->connect();
+					#$sql = "INSERT INTO users(uname, email, password) VALUES (?, ? ,?) ";
+					$sql = $conn->prepare("INSERT INTO customer(username, password, email,phone_no) VALUES (?, ? ,?,?) ");
+					#$sql = $conn->prepare("INSERT INTO users(uname, email, password) VALUES (?, ? ,?) ");
+					$hPassword = password_hash($password, PASSWORD_DEFAULT);
+					//$hPassword = $password;
+					$sql->bind_param("ssss", $username,$hPassword,$uemail,$phone);
+					$sql->execute();
+					$sql->store_result();
+					
+						/*mysqli_stmt_bind_param($stmt, "sss", $username, $uemail,$hPassword);
+						mysqli_stmt_execute($stmt);
+						mysqli_stmt_store_result($stmt);*/
+						header("Location: ../View/login.php");
+						exit();
+
+
+				}
+		}
+
+				mysqli_stmt_close($stmt);
+				mysqli_close($conn);		
+	
+	}
+	
+	public function signup(){
+		
+		$this->signupFunction();
+		
+	}
+}
